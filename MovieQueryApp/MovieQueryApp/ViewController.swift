3cd9ab68645c7bf1a9d89204ce951043f681dda0
milliemince <milliemince4@gmail.com>
2021-01-15 16:02:28 -0600

    comments

--- MovieQueryApp/MovieQueryApp/ViewController.swift
+++ MovieQueryApp/MovieQueryApp/ViewController.swift
@@ -16,8 +16,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var ResultsView: UITableView!
     
-    
-    @IBOutlet weak var Summary: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ResultsView.delegate = self
+        ResultsView.dataSource = self
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return results.count
@@ -36,12 +39,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.queryString = sender.text!
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        ResultsView.delegate = self
-        ResultsView.dataSource = self
-    }
-    
     @IBAction func searchClicked(_ sender: UIButton) {
         let search = toValidSearchString(s: &queryString)
         searchMovies(searchString: search, completion: {
@@ -56,6 +53,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func loadImages(completion: () -> ()) {
+        //had trouble linking UIImage to ImageView in TableViewCell...
         for i in 0...results.count-1 {
             let poster_path_at_index = results[i].poster_path!
             let imageUrlString = "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + poster_path_at_index + ".jpg"
@@ -68,12 +66,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func searchMovies(searchString: String, completion: @escaping () -> ()) {
-        
+        //generate URL
         let urlString = "https://api.themoviedb.org/3/search/movie?api_key=f859ec43c5b76de1d1a8555fa6eb514d&query=" + searchString
-        
         let url = URL(string: urlString)
         
         if (url == nil) {
+            //display alert that input was invalid
             self.displayAlert()
         } else {
         
